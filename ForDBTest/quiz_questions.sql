f2a379fd62ac5535171c8425de6c65bfea8c4069
Memorisza <aut14030@hotmail.com>
2019-11-11 21:38:54 +0700

    [ADD] DBTestingCommand

---
+++ ForDBTest/quiz_questions.sql
@@ -0,0 +1,5 @@
+insert into quiz_questions (question_id, question_title, quiz_id) values (1, 'Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', 1);
+insert into quiz_questions (question_id, question_title, quiz_id) values (2, 'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus.', 1);
+insert into quiz_questions (question_id, question_title, quiz_id) values (3, 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam. Nam tristique tortor eu pede.', 1);
+insert into quiz_questions (question_id, question_title, quiz_id) values (4, 'Suspendisse accumsan tortor quis turpis.', 1);
+insert into quiz_questions (question_id, question_title, quiz_id) values (5, 'Suspendisse potenti. Nullam porttitor lacus at turpis.', 1);
