357a513456d4a6e6be96fa96921c903b81235839
Memorisza <aut14030@hotmail.com>
2019-11-11 19:53:50 +0700

    [UPDATE] PreQuizServlet & QuizServlet(swap)

--- src/java/servlet/PreQuizServlet.java
+++ src/java/servlet/PreQuizServlet.java
@@ -5,12 +5,14 @@
  */
 package servlet;
 
+import controller.QuizController;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Quiz;
 
 /**
  *
@@ -30,18 +32,11 @@ public class PreQuizServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet PreQuizServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet PreQuizServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        int quizId = Integer.parseInt(request.getParameter("quizid"));
+        QuizController qc = new QuizController();
+        Quiz q = qc.findById(quizId);
+        request.setAttribute("quiz", q);
+        getServletContext().getRequestDispatcher("/WEB-INF/view/PreQuiz.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
